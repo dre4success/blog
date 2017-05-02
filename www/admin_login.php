c3ae307a88f8f7965e7b545c5a8e202dcd4f28fe
Damilare Joseph <dre4success@live.com>
2017-05-02 13:35:34 +0100

    form validation

--- www/admin_login.php
+++ www/admin_login.php
@@ -14,18 +14,29 @@
 
 	# error caching
 	$errors = [];
+
+	if(array_key_exists('login', $_POST)){
+
+		if(empty($_POST['email'])){
+			$errors['email'] = "Please Enter Your Email";
+		}
+
+		if(empty($_POST['password'])){
+			$errors['password'] = "Please Enter Your Password";
+		}
+	}
 ?>
 
 <div class="wrapper">
 		<h1 id="register-label">Admin Login</h1>
 				<hr>
-		<form id="register"  action ="login.php" method ="POST">
+		<form id="register"  action ="admin_login.php" method ="POST">
 			<div>
 				<?php
 					if(isset($_GET['msg']))
 					echo '<span class="err">'. $_GET['msg']. '</span>';
 					
-						$display = displayErrors($errors, 'email');
+						$display = Tools::DisplayErrors($errors, 'email');
 						echo $display;
 
 				?>
@@ -34,19 +45,16 @@
 			</div>
 			<div>
 				<?php
-					if(isset($_GET['msg']))
-					echo '<span class="err">'. $_GET['msg']. '</span>';
-					
-					$display = displayErrors($errors, 'password');
+					$display = Tools::DisplayErrors($errors, 'password');
 					echo $display;
 				?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
 
-			<input type="submit" name="register" value="login">
+			<input type="submit" name="login" value="login">
 		</form>
 
-		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
+		<h4 class="jumpto">Don't have an account? <a href="admin_register.php">register</a></h4>
 		</div>
 
