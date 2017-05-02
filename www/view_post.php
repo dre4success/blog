7a9c0b78d495329db6dfc45d9020999b15a990e8
Damilare Joseph <dre4success@live.com>
2017-05-02 18:15:27 +0100

    view post showing

--- www/view_post.php
+++ www/view_post.php
@@ -15,3 +15,30 @@ session_start();
 	# include header
 	include 'includes/view.php';
 
+?>
+<div class="wrapper">
+				<h1 id="register-label">View Post</h1>
+				<hr>
+				<div id="stream">
+
+			<table id="tab">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Blogger</th>
+						<th>Content</th>
+						<th>Date</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+				
+						<?php
+								$view = Tools::viewPost($conn);
+								echo $view;
+						?>
+
+          		</tbody>
+			</table>
+		</div>
