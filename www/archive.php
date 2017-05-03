e1fe086d54690cc888057b994e17287d4f91431e
Damilare Joseph <dre4success@live.com>
2017-05-03 17:50:50 +0100

    getting from archive

--- www/archive.php
+++ www/archive.php
@@ -1,1 +1,14 @@
 <?php
+
+	include 'includes/front_header.php';
+
+	include 'includes/functions.php';
+
+	include 'includes/db.php';
+
+	if(isset($_GET['post_id'])){
+
+		$postID = $_GET['post_id'];
+	}
+
+?>
